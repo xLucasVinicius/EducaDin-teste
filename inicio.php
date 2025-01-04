20a27f28040d5dd448e51786d6eb245ac1bc7ed9
Lucas Vinicius <lucas.joanico13@gmail.com>
2025-01-04 17:31:24 -0300

    testes

--- inicio.php
+++ inicio.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EducaDin</title>
-    <link rel="shortcut icon" href="../imagens/logos/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./imagens/logos/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./Style/inicio.css">
 </head>
 <body>
-    
+    <section class="content">
+        <div class="introducao">
+            <h1>Seja bem-vindo ao <span>EducaDin!</span></h1>
+        </div>
+        <div class="links">
+            <a href="login.php">Login</a>
+            <a href="cadastro.php">Cadastro</a>
+        </div>
+        <div class="img-edu">
+            <img src="./imagens/logos/favicon.png" alt="Logo EducaDin">
+        </div>
+    </section>
 </body>
 </html>
